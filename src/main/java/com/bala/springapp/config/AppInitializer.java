8575dca078a4906a4bdcfd1c611214a96bdb6844
devuser <karjule128@gmail.com>
2017-10-15 14:07:58 +0530

    small error removed and home.jsp changed

--- src/main/java/com/bala/springapp/config/AppInitializer.java
+++ src/main/java/com/bala/springapp/config/AppInitializer.java
@@ -5,6 +5,7 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
 
 import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
@@ -20,7 +21,7 @@ public class AppInitializer implements WebApplicationInitializer {
 
 		// Create the dispatcher servlet's Spring application context
         AnnotationConfigWebApplicationContext dispatcherServlet = new AnnotationConfigWebApplicationContext();
-        dispatcherServlet.register(MvcConfig.class);
+        dispatcherServlet.register(AppConfig.class);
              
         // Register and map the dispatcher servlet
         ServletRegistration.Dynamic dispatcher = container.addServlet("dispatcher", new DispatcherServlet(dispatcherServlet));
--- src/main/webapp/WEB-INF/jsp/home.jsp
+++ src/main/webapp/WEB-INF/jsp/home.jsp
@@ -5,10 +5,15 @@
 	<title>Home</title>
 </head>
 <body>
-<h1>
-	Hello world!  
-</h1>
 
-<P>  The time on the server is ${serverTime}. </P>
+<br>
+
+<br>
+
+<h5>
+	Welcome ! This is sample web SpringMvcAnnotationBased  application. 
+</h5>
+
+<P> <h5> The time on the server is <h5> <h4>${serverTime}.<h4> </P>
 </body>
 </html>
